901b8f192d5040d76724bccd00ca80fc32a29cf1
JiamingSitu <94401961+JiamingSitu@users.noreply.github.com>
2022-04-25 12:58:55 -0700

    asdfg

--- src/Chess.java
+++ src/Chess.java
@@ -1,38 +1,15 @@
-import java.awt.Graphics;
-import java.awt.Color;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 
-public class Chess extends JPanel {
 
-	Chessboard CB;
-	
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		
-		int w = getHeight();
-		int h = getWidth();
-		
-		
-		
-		
-	}
+public class Chess {
+
 	
 	public static void main (String[] args) {
 		
-	//CB = new ChessBoard(1, 2, 3, 4)
+		Chessboard CB = new Chessboard(123, 456);
+		CB.drawBoard();
 		
 		
-		
-		
-		
-		JFrame window = new JFrame("Chessboard");
-		window.setBounds(100, 100, 600, 600);
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		JPanel canvas = new Chess();
-		canvas.setBackground(Color.WHITE);
-		window.getContentPane().add(canvas);
-		window.setVisible(true);
+
 		
 	}
 }
--- src/Chessboard.java
+++ src/Chessboard.java
@@ -1,15 +1,47 @@
+import java.awt.Graphics;
+import java.awt.Color;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class Chessboard {
-	public int gridWidth, gridHeight, boardWidth, boardHeight;
+public class Chessboard extends JPanel {
+	public final int WINDOW_SIZE = 500;
+	public int boardWidth, boardHeight; // in tiles
+	private int gridSize; // in pixels
 	
-	public Chessboard(int gW, int gH, int bW, int bH) {
-		gridWidth = gW;
-		gridHeight = gH;
+	
+	public Chessboard(int bW, int bH) {
 		boardWidth = bW;
 		boardHeight = bH;
+		gridSize = 500 / Math.min(boardWidth, boardHeight);
 	}
 	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		int w = getHeight();
+		int h = getWidth();
+		
+		for (int i = 0; i < boardHeight; i++) {
+			for (int j = 0; j < boardWidth; j++) {
+				
+				g.drawRect(i * gridSize, j * gridSize, gridSize, gridSize);
+				
+			}
+		}
+		
+		
+	}
 	
+	public void drawBoard() {
+		
+		JFrame window = new JFrame("Chessboard");
+		window.setBounds(100, 100, 100 + WINDOW_SIZE, 100 + WINDOW_SIZE);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setBackground(Color.WHITE);
+		window.getContentPane().add(this);
+		window.setVisible(true);
+		
+	}
 	
 	
 	

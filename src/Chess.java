55c2f402512b89ef00ed99dc34ca595f06fe7d5f
JiamingSitu <94401961+JiamingSitu@users.noreply.github.com>
2022-04-25 12:49:24 -0700

    asdfg

--- src/Chess.java
+++ src/Chess.java
@@ -1,13 +1,38 @@
-public class Chess {
+import java.awt.Graphics;
+import java.awt.Color;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-	public static void main (String[] args) {
+public class Chess extends JPanel {
+
+	Chessboard CB;
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
 		
-		System.out.println("asd");
+		int w = getHeight();
+		int h = getWidth();
+		
 		
 		
 		
 	}
 	
-	
-	
+	public static void main (String[] args) {
+		
+	//CB = new ChessBoard(1, 2, 3, 4)
+		
+		
+		
+		
+		
+		JFrame window = new JFrame("Chessboard");
+		window.setBounds(100, 100, 600, 600);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JPanel canvas = new Chess();
+		canvas.setBackground(Color.WHITE);
+		window.getContentPane().add(canvas);
+		window.setVisible(true);
+		
+	}
 }
---
+++ src/Chessboard.java
@@ -0,0 +1,17 @@
+
+public class Chessboard {
+	public int gridWidth, gridHeight, boardWidth, boardHeight;
+	
+	public Chessboard(int gW, int gH, int bW, int bH) {
+		gridWidth = gW;
+		gridHeight = gH;
+		boardWidth = bW;
+		boardHeight = bH;
+	}
+	
+	
+	
+	
+	
+	
+}
